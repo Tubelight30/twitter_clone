27ad3d02dc102c514086ade112c979f8cde5f2c1
SaumyaGupta <saumyagupta@debianPC>
2023-08-30 15:38:35 +0530

    First commit

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,6 @@
+class AppwriteConstants {
+  static const String databaseId = '64ef1174a443b1c45a0b';
+  static const String projectId = '64e1db63df0354c2f3c6';
+  static const String endPoint =
+      'http://localhost:80/v1'; //this helps flutter communicate with appwrite
+}
