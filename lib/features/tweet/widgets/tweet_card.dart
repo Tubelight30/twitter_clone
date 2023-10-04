7c5010dda630f843e94f9cba1c4b72a8d16e6a17
Tubelight30 <soumaygupta2004@gmail.com>
2023-10-05 00:43:52 +0530

    Update feedScreen UI
    tweets along with images are shown

--- lib/features/tweet/widgets/tweet_card.dart
+++ lib/features/tweet/widgets/tweet_card.dart
@@ -2,8 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:twitter_clone/common/common.dart';
 import 'package:twitter_clone/common/error_page.dart';
+import 'package:twitter_clone/core/enums/tweet_type_enum.dart';
 import 'package:twitter_clone/features/auth/controller/auth_controller.dart';
+import 'package:twitter_clone/features/tweet/widgets/carousel_image.dart';
+import 'package:twitter_clone/features/tweet/widgets/hashtag_text.dart';
 import 'package:twitter_clone/models/tweet_model.dart';
+import 'package:twitter_clone/theme/theme.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
 class TweetCard extends ConsumerWidget {
   final Tweet tweet;
@@ -20,8 +25,49 @@ class TweetCard extends ConsumerWidget {
               children: [
                 Row(
                   children: [
-                    CircleAvatar(
-                      backgroundImage: NetworkImage(user.profilePic),
+                    Container(
+                      margin: const EdgeInsets.all(10),
+                      child: CircleAvatar(
+                        backgroundImage: NetworkImage(user.profilePic),
+                        radius: 35,
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          //need to add retweeted
+                          Row(
+                            children: [
+                              Container(
+                                margin: const EdgeInsets.only(right: 5),
+                                child: Text(
+                                  user.name,
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 19,
+                                  ),
+                                ),
+                              ),
+                              Text(
+                                '@${user.name}• ${timeago.format(
+                                  tweet.tweetedAt,
+                                  locale: 'en_short',
+                                )}',
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Pallete.greyColor,
+                                  fontSize: 17,
+                                ),
+                              ),
+                            ],
+                          ),
+                          //need to add replied to
+                          HashtagText(text: tweet.text),
+                          if (tweet.tweetType == TweetType.image)
+                            CarouselImage(imageLinks: tweet.imagesLinks),
+                        ],
+                      ),
                     ),
                   ],
                 ),
